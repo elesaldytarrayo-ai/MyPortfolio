32a15e3b6b794cf18fe83dbf8885a08e24e8754c
elesaldyrosello-hub <elesaldyrosello@gmail.com>
2026-09-20 11:16:08 +0800

    adding my education background

---
+++ my-portfolio/src/Components/Education.jsx
@@ -0,0 +1,50 @@
+function Education() {
+  return (
+    <section className="page-section">
+
+      <div className="section-heading">
+        <span>03</span>
+        <h1>Education</h1>
+        <p>My academic background.</p>
+      </div>
+
+      <div className="education-card">
+
+        <div className="education-icon">
+          IT
+        </div>
+
+        <div>
+          <span className="date-label">
+            CURRENT EDUCATION
+          </span>
+
+          <h2>
+            Bachelor of Science in Information Technology
+          </h2>
+
+          <h3>
+            Northwest Samar State University
+          </h3>
+
+          <p>
+            Bachelor of Science in Information Technology student
+            developing knowledge in programming, database management,
+            web development, networking, and information systems.
+          </p>
+
+          <div className="education-tags">
+            <span>BSIT</span>
+            <span>Programming</span>
+            <span>Database</span>
+            <span>Web Development</span>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Education;
